770baccfe6e434b2460b76680463826e96a3780c
halaabubaker9 <83541956+halaabubaker9@users.noreply.github.com>
2021-05-30 11:30:56 +0300

    First commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>Import File</title>
+</head>
+<body>
+@include('Navbar')
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
+
+{{-- <style>
+    html,body {
+    height: 100%;
+    }
+    </style>
+    <div class="container h-100">
+    <div class="row h-100">
+        <div class="card col-4 mx-auto my-auto align-middle">
+        <div class="card-body">
+            <h5 class="card-title">Login</h5>
+            <form method="POST">
+                @csrf
+            <div class="form-group">
+                <label for="user">username</label>
+                <input type="email" class="form-control" id="user">
+            </div>
+            <div class="form-group">
+                <label for="password">password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary float-right">Login</button>
+            </form>
+        </div>
+        </div>
+    </div>
+    </div> --}}
